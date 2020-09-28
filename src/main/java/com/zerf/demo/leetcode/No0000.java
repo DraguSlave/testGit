322a357beca9199cf6cc0f4ca2c3f1bd5b43e793
zj <zj@qq.com>
2020-09-28 16:59:50 +0800

    hello world

---
+++ src/main/java/com/zerf/demo/leetcode/No0000.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C), 2015-2020, XXX有限公司
+ * FileName: No0000
+ * Author:   Administrator
+ * Date:     20/9/28/0028 16:57
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+package com.zerf.demo.leetcode;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ *
+ * @author Administrator
+ * @create 20/9/28/0028
+ * @since 1.0.0
+ */
+public class No0000 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
